f64f186f94c9514bb547f7feb84a4d1d9fa94807
Snehal Tikole <126760961+Snehaltikole14@users.noreply.github.com>
2025-12-22 13:16:38 +0530

    Update UserRepository.java

--- src/main/java/com/example/Dr/VehicleCare/repository/UserRepository.java
+++ src/main/java/com/example/Dr/VehicleCare/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.example.Dr.VehicleCare.repository;
 
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.Dr.VehicleCare.model.User;
+import com.example.Dr.VehicleCare.model.enums.UserRole;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
@@ -13,4 +15,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     
     
     Optional<User> findByEmailOrName(String email, String name);
+    long countByRole(UserRole role);
+    List<User> findByRole(UserRole role);
+
 }
