d4b5ca012ba0c9425de78374660be5afd57881de
Snehal Tikole <126760961+Snehaltikole14@users.noreply.github.com>
2025-12-22 13:08:46 +0530

    Update BookingRepository.java

--- src/main/java/com/example/Dr/VehicleCare/repository/BookingRepository.java
+++ src/main/java/com/example/Dr/VehicleCare/repository/BookingRepository.java
@@ -1,12 +1,20 @@
 package com.example.Dr.VehicleCare.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+
 import com.example.Dr.VehicleCare.model.Booking;
+import com.example.Dr.VehicleCare.model.enums.PaymentStatus;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+
+    List<Booking> findByPaymentStatusAndCreatedAtBetween(PaymentStatus status, LocalDateTime start, LocalDateTime end);
+    List<Booking> findByPaymentStatusAndCreatedAtAfter(PaymentStatus status, LocalDateTime from);
+    List<Booking> findByPaymentStatus(PaymentStatus status);
     List<Booking> findByUserId(Long userId);
-    List<Booking> findByStatus(String status);
+
 }
