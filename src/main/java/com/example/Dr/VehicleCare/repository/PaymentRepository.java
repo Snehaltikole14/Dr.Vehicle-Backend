6f9670966276fc1dddabf73ccf76e3345474f210
Snehal Tikole <126760961+Snehaltikole14@users.noreply.github.com>
2026-01-09 11:48:37 +0530

    Update PaymentRepository.java

--- src/main/java/com/example/Dr/VehicleCare/repository/PaymentRepository.java
+++ src/main/java/com/example/Dr/VehicleCare/repository/PaymentRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.Dr.VehicleCare.model.Payment;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+  Payment findByRazorpayOrderId(String razorpayOrderId);
+    List<Payment> findByStatus(PaymentStatus status);
 }
+
