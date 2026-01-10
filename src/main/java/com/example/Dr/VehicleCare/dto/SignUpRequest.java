f8299170d3a8a52de665f95623d0ae0293901c1e
Snehal Tikole <126760961+Snehaltikole14@users.noreply.github.com>
2026-01-10 11:44:05 +0530

    Update SignUpRequest.java

--- src/main/java/com/example/Dr/VehicleCare/dto/SignUpRequest.java
+++ src/main/java/com/example/Dr/VehicleCare/dto/SignUpRequest.java
@@ -9,10 +9,11 @@ import lombok.Setter;
 @Getter @Setter @NoArgsConstructor
 public class SignUpRequest {
     @NotBlank private String name;
-    @Email @NotBlank private String email;
+
     @NotBlank private String password;
     private String phone;
     private String role;
     @NotBlank
     private String otp;// optional, default CUSTOMER
 }
+
