90799181d11d3bd750c49b5b6e68c53aeaaa92b4
Snehal Tikole <126760961+Snehaltikole14@users.noreply.github.com>
2026-01-13 11:53:50 +0530

    Update LoginRequest.java

--- src/main/java/com/example/Dr/VehicleCare/dto/LoginRequest.java
+++ src/main/java/com/example/Dr/VehicleCare/dto/LoginRequest.java
@@ -5,6 +5,7 @@ package com.example.Dr.VehicleCare.dto;
 import lombok.*;
 @Getter @Setter @NoArgsConstructor
 public class LoginRequest {
-    private String email;
+ private String identifier;
     private String password;
 }
+
